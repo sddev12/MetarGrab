9d162942e3b1d736e644e0e26b8b31cf6465d176
sddev12 <sd.dev@outlook.com>
2023-02-04 19:14:49 +0000

    tidied up tf and added pipeline yml

--- deploy/terraform/linux_web_app.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_linux_web_app" "metargrab_web_app" {
-  name            = "metargrab-${var.region_short}-${var.environment_short}-lwa"
-  location        = azurerm_resource_group.metargrab.location
-  service_plan_id = azurerm_service_plan.metargrab_serviceplan.id
-
-  site_config {
-
-  }
-}
